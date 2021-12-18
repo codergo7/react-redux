a897d0bf66cbd64c6b1fb0fd49225d10e2cd3d6d
codergo7 <codergo7@gmail.com>
2021-12-18 16:03:59 +0100

    modified reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -3,12 +3,12 @@ import { combineReducers } from 'redux'
 import todosReducer from './features/todos/todosSlice'
 import filtersReducer from './features/filters/filtersSlice'
 
-const rootReducer = combineReducers(state = {}, action) {
+const rootReducer = combineReducers({
   // always return a new object for the root state
  
     todos: todosReducer,
     filters: filtersReducer
-}
+})
 
 export default rootReducer;
 
