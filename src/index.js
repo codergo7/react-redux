679d7d21118b12e22608190eb02e71c136f178f5
codergo7 <codergo7@gmail.com>
2021-12-20 21:35:16 +0100

    add actions using dispatch

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App'
 
 import './api/server'
+import store from './store'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,3 +12,29 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 )
+
+//log the initial state
+console.log('Initial state: ', store.getState())
+// {todos: [....], filters: {status, colors}}
+
+// Every time the state changes, log it
+// Note that subscribe() returns a function for unregistering the lsitener
+
+const unsubscribe = store.subscribe( ()=> 
+console.log('State after dispatch: ', store.getState())
+)
+
+store.dispatch({type: 'todos/todoAdded', payload: 'Learn about actions'})
+store.dispatch({type: 'todos/todoAdded', payload: 'Learn about reducers'})
+store.dispatch({type: 'todos/todoAdded', payload: 'Learn about stores'})
+
+store.dispatch({type:'todos/todoToggled', payload: 0})
+store.dispatch({type:'todos/todoToggled', payload: 1})
+
+store.dispatch({type: 'filters/statusFilterChanged', payload: 'Active'})
+
+store.dispatch( {type: 'filters/colorFilterChanged', payload: {color:'red', changeType: 'added'}})
+
+// Dispatch one more action to see what happens
+
+store.dispatch({ type: 'todos/todoAdded', payload: 'Try creating a store' })
